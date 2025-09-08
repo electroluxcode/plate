f46f81ed3c9177de27a48b4a4a5be322a5a6186d
electroluxcode <electroluxcoder@gmail.com>
2025-09-08 16:58:49 +0800

    fix: build

--- ADAPT.sh
+++ ADAPT.sh
@@ -1,4 +1,4 @@
 # yarn build
 cd apps/plate-editor
-yarn install @semantic-release/changelog@6 @semantic-release/git@10 @semantic-release/gitlab@9 @semantic-release/npm@9
+yarn add @semantic-release/changelog@6 @semantic-release/git@10 @semantic-release/gitlab@9 @semantic-release/npm@9
 yarn semantic-release
