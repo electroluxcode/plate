21779b5bca8fe4b0c24f1a119621310a21cea58d
electroluxcode <electroluxcoder@gmail.com>
2025-09-08 17:05:47 +0800

    fix: build

--- ADAPT.sh
+++ ADAPT.sh
@@ -3,8 +3,8 @@
 # 切换到 plate-editor 目录
 cd apps/plate-editor
 
-# 临时安装所有必需的 semantic-release 插件
-npm install --no-save \
+# 临时安装所有必需的 semantic-release 插件，使用 --legacy-peer-deps 解决依赖冲突
+npm install --no-save --legacy-peer-deps \
   semantic-release \
   @semantic-release/changelog \
   @semantic-release/git \
@@ -12,4 +12,4 @@ npm install --no-save \
   @semantic-release/npm
 
 # 执行 semantic-release
-npx semantic-release
+./node_modules/.bin/semantic-release
